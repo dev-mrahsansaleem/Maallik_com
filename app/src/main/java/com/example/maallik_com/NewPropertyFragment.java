7c828bb80d83aca40c4be13b6170f8197c29201c
ahsansaleem <ahsansaleem6389@gmail.com>
2020-06-08 11:48:39 +0500

    create custom menu with NewProperty fragment

---
+++ app/src/main/java/com/example/maallik_com/NewPropertyFragment.java
@@ -0,0 +1,20 @@
+package com.example.maallik_com;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class NewPropertyFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_new_property, container, false);
+
+        return v;
+    }
+}
